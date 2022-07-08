dd3d7e8958306051c2fa55f4a7e0d15e8a25c25f
Jaime Andres Herrera Estrada <jherrerah@summa-sci.com>
2022-07-08 15:21:47 -0500

    actualizacion puerto

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(process.env.PORT || 3000, () => console.log("Server running in port 3000"))
+
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando');
+// });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
